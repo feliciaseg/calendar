593a942f6ba7bca6234da598e2d3fefeb183926e
mariahelenanoren <marialinder97@hotmail.com>
2020-11-19 12:08:14 +0100

    added filler divs before and after calendar days

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 window.onload = main;
 
-function main() {
-    getApi();
-    getMonth();
+async function main() {
+    getSvenskaDagarApi();
+    getCurrentMonth();
     createCalendarDays();
 }
 
-async function getApi() {
+async function getSvenskaDagarApi() {
+    const previousMonth = getPreviousMonth();
+    const currentMonth = getCurrentMonth();
+    const nextMonth = getNextMonth();
+
+    getApiForPreviousMonth(previousMonth);
+    const currentMonthsData = await getApiForCurrentMonth(currentMonth);
+    console.log("currentMonthsData"  + currentMonthsData)
+    getApiForNextMonth(nextMonth);
+    createCalendarDays(currentMonthsData)
+}
+
+async function getApiForPreviousMonth(previousMonth) {
     try {
-        const month = getMonth();
+        const month = previousMonth;
         const result = await fetch("https://sholiday.faboul.se/dagar/v2.1/2020/" + month)
         const data = await result.json();
         return data;
@@ -18,28 +30,126 @@ async function getApi() {
     }
 }
 
-function getMonth() {
-    const date = new Date();
-    const month = date.getMonth() + 1;
-    return month;
+async function getApiForCurrentMonth(currentMonth) {
+    try {
+        const month = currentMonth;
+        const result = await fetch("https://sholiday.faboul.se/dagar/v2.1/2020/" + month)
+        const data = await result.json();
+        return data;
+    }
+    catch(error) {
+        console.error(error)
+    }
 }
 
-async function createCalendarDays() {
-    const data = await getApi();
-    const dagar = data.dagar
-    console.log(data)
+async function getApiForNextMonth(nextMonth) {
+    try {
+        const month = nextMonth;
+        const result = await fetch("https://sholiday.faboul.se/dagar/v2.1/2020/" + month)
+        const data = await result.json();
+        return data;
+    }
+    catch(error) {
+        console.error(error)
+    }
+}
 
+function getPreviousMonth() {
+    const date = new Date();
+    const previousMonth = date.getMonth();
+    return previousMonth;
+}
+
+function getCurrentMonth() {
+    const date = new Date();
+    const currentMonth = date.getMonth() + 1;
+    return currentMonth;
+}
+
+function getNextMonth() {
+    const date = new Date();
+    const nextMonth = date.getMonth() + 2;
+    return nextMonth;
+}
+
+function createCalendarDays(currentMonthsData) {
     const calendar = document.getElementById("calendar")
-    
-    for (dag in dagar) {
-        const div = document.createElement("div")
-        const date = document.createElement("p")
-        date.innerHTML = Number(dag) + 1;
-        calendar.append(div)
-        div.append(date)
+    const data = currentMonthsData;
 
-        if (dagar[dag].veckodag === "Söndag") {
-            div.style.backgroundColor = "gray"
+    if (data) {
+        const dagar = data.dagar;
+        const firstDayInMonth = dagar[0]
+        addFillerDivsBeforeCalendarDays(firstDayInMonth);
+
+        for (dag in dagar) {
+            const div = document.createElement("div")
+            const date = document.createElement("p")
+            date.innerHTML = Number(dag) + 1;
+            calendar.append(div)
+            div.append(date)
+    
+            if (dagar[dag].veckodag === "Söndag") {
+                div.style.backgroundColor = "gray"
+            }
+        }
+        addFillerDivsAfterCalendarDays();
+    }
+}
+
+function addFillerDivsBeforeCalendarDays(firstDayInMonth) {
+    const calendar = document.getElementById("calendar")
+
+    fillerDivs = {
+        "Måndag": 0,
+        "Tisdag": 1,
+        "Onsdag": 2,
+        "Torsdag": 3,
+        "Fredag": 4,
+        "Lördag": 5,
+        "Söndag": 6
+    }
+
+    for (number in fillerDivs) {
+        if (firstDayInMonth.veckodag === number) {
+            for (i = 0; i < fillerDivs[number]; i++) {
+                const fillerDiv = document.createElement("div");
+                fillerDiv.style.backgroundColor = "lightGrey";
+                calendar.append(fillerDiv)
+            }
         }
     }
+}
+
+function addFillerDivsAfterCalendarDays() {
+    const calendar = document.getElementById("calendar")
+    const totalGridCapacity = calculateCalendarGrid(calendar);
+    const currentDivsCount = calendar.children;
+    const emptySlots = totalGridCapacity - currentDivsCount.length;
+
+    for (i = 0; i < emptySlots; i++) {
+        const fillerDiv = document.createElement("div");
+        fillerDiv.style.backgroundColor = "lightGrey";
+        calendar.append(fillerDiv)
+    }
+}
+
+function calculateCalendarGrid(calendar) {
+    let gridColumnStyle = getComputedStyle(calendar).gridTemplateColumns;
+    let gridRowStyle = getComputedStyle(calendar).gridTemplateRows;
+
+    gridColumn = gridColumnStyle.split(" ");
+    gridRow = gridRowStyle.split(" ");
+
+    gridColumnIndex = 0;
+    gridRowIndex = 0;
+
+    for (arrayObject in gridColumn) {
+        gridColumnIndex +=1;
+    }
+    for (arrayObject in gridRow) {
+        gridRowIndex +=1;
+    }
+
+    const totalGridCapacity = gridColumnIndex * gridRowIndex;
+    return totalGridCapacity;
 }

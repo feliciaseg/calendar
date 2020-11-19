81df50e43eb98cf2e0ca34d6882f648e9e897076
Felicia Segerholm <feliciasegerholm@Felicias-MacBook-Air-3.local>
2020-11-19 17:16:45 +0100

    förbättringar diskuterade under samtalet gjord

--- main.js
+++ main.js
@@ -3,20 +3,21 @@ window.addEventListener ("load", main)
 function main() {
     currentTime();
     currentDate();
+    
 }
 
+let date = new Date();
+
 //Funktionen läggs till eftersom t.ex 07:03 visas som 7:3, därför behöver vi lägga till en 0:a
-function addZero(a) {
-    if (a < 10){
-        a = "0" + a;
+function addZero(timevalue) {
+    if (timevalue < 10){
+        timevalue = "0" + timevalue;
     }
-    return a;
+    return timevalue;
 }
 
 function currentTime(){
-    // Jag har let date variabeln i function-scopet istället för det globala, ska jag ändra det?
-    let date = new Date();
-
+    
     let hours = addZero(date.getHours());
     let minutes = addZero(date.getMinutes());
     let time = hours + ":" + minutes
@@ -26,12 +27,11 @@ function currentTime(){
     showTime.innerHTML = time;
 }
 setInterval (currentTime, 1000) 
-// Kommentera gärna här ovan om jag kan skriva setInterval funktionen på annat sätt, med längre tid eller liknande 
+
 
 
 function currentDate(){
-    let date = new Date();
-
+    
     // Veckodagen
     let NumberOfday = date.getDay();
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
@@ -52,7 +52,6 @@ function currentDate(){
     let showDate = document.getElementById("showDate")
     showDate.innerHTML = todaysDate;
 }
-
-// Behöver jag ett set-interval här?
+// Behöver vi ett set-interval här?
 
 

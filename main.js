9c987ddcb8040c959a0b6c34f94e757c7975b2ab
mariahelenanoren <marialinder97@hotmail.com>
2020-11-20 10:48:18 +0100

    changed divs structure

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-
-var dateControl = document.querySelector('input[type="date"]');
-dateControl.value = '2017-06-01';
-console.log(dateControl.value); // prints "2017-06-01"
-console.log(dateControl.valueAsNumber); // prints 1496275200000, a JavaScript timestamp (ms)
-
-var timeControl = document.querySelector('input[type="time"]');
-timeControl.value = '15:30';
-
 window.addEventListener ("load", main)
 
 function main() {

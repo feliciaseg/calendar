daefc296ca2d8c570b0a8ea6dee9f8cb4de9b43b
Felicia Segerholm <feliciasegerholm@Felicias-MacBook-Air-3.local>
2020-11-20 14:34:02 +0100

    la till funktionen för att öppna ''new-task'' boxen när man klickar på knappen

--- to-do.js
+++ to-do.js
@@ -1,2 +1,28 @@
-window.addEventListener ("load", main)
+
+ //Selektorer
+const timePicked = document.getElementById("timePicked");
+const datePicked = document.getElementById("datePicked");
+const description = document.getElementById("description");
+const btnAddNewItem = document.getElementById("addNewItem"); //Knappen för att lägga in input till en ny ''Todo''
+const toDoList = document.getElementById("to-do-list");
+const btnOpenNewTask = document.getElementById("openNewTask");
+const newTaskDiv = document.getElementById("newTaskDiv");
+const primaryContentDiv = document.getElementById("primaryContentDiv")
+
+
+
+//Event listeners
+
+btnOpenNewTask.addEventListener('click', openNewTaskDiv)
+
+
+// //Functions
+
+ function openNewTaskDiv(){
+     if (newTaskDiv.classList.contains('none')){
+         newTaskDiv.classList.remove('none');
+         primaryContentDiv.classList.add('none');
+     }
+
+ }
 

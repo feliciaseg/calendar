cc8a60e0e02da74e9ca2fdff6fba5088b2619afb
Felicia Segerholm <feliciasegerholm@Felicias-MacBook-Air-3.local>
2020-11-24 01:03:03 +0100

    funktionen fungerar!!

--- to-do.js
+++ to-do.js
@@ -1,85 +1,87 @@
 
+/** Sets function to run on window load */
+window.onload = todoMain;
 
- //Selektorer
-
-const btnAddNewItem = document.getElementById("addNewItem"); //Knappen för att lägga in input till en ny ''Todo''
-const btnOpenNewTask = document.getElementById("openNewTask");
-
-
-//Event listeners
-btnOpenNewTask.addEventListener("click", openNewTaskDiv)
-btnAddNewItem.addEventListener ("click", addToDo)
-
-
-
-// //Functions
-
-const newTaskDiv = document.getElementById("newTaskDiv");
-const primaryContentDiv = document.getElementById("primaryContentDiv")
-
-//Funktion för att öppna ''new task''diven. Steg 1.
-function openNewTaskDiv(){
-   
-    if (newTaskDiv.classList.contains("none")){
-        newTaskDiv.classList.remove("none");
-        primaryContentDiv.classList.add("none");
-     }
-     
+/** Functions to run on window load */
+function todoMain() {
+    addEventListeners();
+    showTodos();
 }
 
-//Funktion för att spara value( dvs inputen som läggs i formuläret)
-let tasks = [] //Array för att spara inputen i
- function addToDo(event) {
+/** Adds event listeners */
+function addEventListeners() {
+    const btnOpenNewTask = document.getElementById("openNewTask");
+    btnOpenNewTask.addEventListener("click", openNewDiv);
+
+    const btnAddItem = document.getElementById("addNewItem");
+    btnAddItem.addEventListener("click", addNewItem)
+}
+
+
+/** Displays or hide the divs*/
+function openNewDiv(){
+    newTaskDiv = document.getElementById("newTaskDiv")
+    primaryDiv =  document.getElementById("primaryContentDiv")
+
+    newTaskDiv.classList.toggle("none");
+    primaryDiv.classList.toggle("none");
+}
+
+
+/** Saves input value from the form */
+let tasks = []
+function addNewItem(event){
+    event.preventDefault();
     
-    event.preventDefault(); //För att stoppa sidan från att ladda om vid tryck på knappen
-    //Nedan objektet som görs för nytt input. 
-    let task = { 
+    let task = {
         date: document.getElementById("datePicked").value,
         time: document.getElementById ("timePicked").value,
         description: document.getElementById("description").value
     }
-    tasks.push(task); //pushar in objektet i arrayen.
-    
-    // Spara i local storage 
+    tasks.push(task);
     localStorage.setItem("savedTasks", JSON.stringify(tasks));
-    
-    openPrimaryContentDiv();
+    saveToLS();
+    openNewDiv();
 }
 
- // Funktion för att "framsidan"
-
-function openPrimaryContentDiv(){
-    if (primaryContentDiv.classList.contains("none")){
-        primaryContentDiv.classList.remove("none");
-        newTaskDiv.classList.add("none");
-    } 
-    
+/** Saves the input to Local Storage */
+function saveToLS() {
+    localStorage.setItem("savedTasks", JSON.stringify(tasks));
+    showTodos();
 }
 
-// Funktion för att visa sparat i Local storage
+/** Empties the todo container*/
+function emptyTodoContainer(){
+    const container = document.getElementById ("todos");
+    let containerChildren = container.children;
+   
+    while (containerChildren.length > 0) {
+        containerChildren[0].remove()
+    }
 
- function showTodos() {
-    
-    //Hämtar från från LS
-    let savedTasks = JSON.parse(localStorage.getItem("savedTasks"));
-    
-    const todoContainer = document.getElementById("todos");
-    //hämta containern
+}
 
-    for (task in tasks) {
-        //skapar element.. 
+/** Shows all saved tasks/todos */
+function showTodos() {
+     emptyTodoContainer();
+
+     const savedTasks = JSON.parse(localStorage.getItem("savedTasks"));
+     const todoContainer = document.getElementById("todos");
+     
+     for (task in savedTasks) {
         const div = document.createElement("div");
         const pTime = document.createElement("p");
         const pDescription = document.createElement ("p");
-
-        // appendar element till varandra
-        todoContainer.append(div)
+    
+         // appendar element till varandra
+        todoContainer.append(div);
         div.append(pTime);
         div.append(pDescription);
+    
+         // vad som ska stå i elementen
+        pTime.innerHTML = (savedTasks[task].time);
+        pDescription.innerHTML = (savedTasks[task].description);  
+    } 
+  
+} 
 
-        // vad som ska stå i elementen
-        pTime.innerHTML = (tasks[task].time);
-        pDescription.innerHTML = (tasks[task].description);
-    }
-
- }
